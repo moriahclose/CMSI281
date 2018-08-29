e5e2466f20661ae407c5ffad8b2b6bb74ab260eb
Moriah Tolliver <mtollive@lion.lmu.edu>
2018-08-29 13:41:31 -0700

    moved SayHello.java to Classwork

---
+++ Classwork/Classwork01/SayHello.java
@@ -0,0 +1,14 @@
+/*****************************************************************************
+Name: Moriah Tolliver         Date: 01/15/18          Purpose: Greet the user
+Referenced Greeter.java from my own CMSI186 repository
+******************************************************************************/
+import java.util.Scanner;
+
+public class SayHello {
+   public static void main( String args[] ) {
+       Scanner nameScanner = new Scanner(System.in);
+       System.out.println( "What is your name?" );
+       String userName = nameScanner.next();
+      System.out.println( "\n  Hello, " + userName + "!" );
+   }
+}
