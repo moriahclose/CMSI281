5fc3d905969f926006b6d2fcb26e21ecf9e623ad
Moriah Tolliver <mtollive@lion.lmu.edu>
2018-09-14 11:59:34 -0700

    HighArray.java corrected

--- Homework/Homework01/HighArray.java
+++ Homework/Homework01/HighArray.java
@@ -6,7 +6,7 @@ Purpose Demonstrates array class with high-level interface
 
 class HighArray {
     private long[] a;   // ref to array a
-    private in nElems;  // number of data items
+    private int nElems;  // number of data items
 
     public HighArray( int max ) {
         a = new long[max];
@@ -17,12 +17,9 @@ class HighArray {
         int j;
         for ( j = 0; j < nElems; j++ ) {
             if ( a[j] == searchKey )
-                break;
-            if ( j == nElems )
-                return false;
-            else
                 return true;
         }
+        return false;
     }
 
     public void insert( long value ) {
@@ -33,21 +30,50 @@ class HighArray {
     public boolean delete( long value ) {
         int j;
         for ( j = 0; j < nElems; j++ ) {
-            if ( value == a[j] )
-                break;
-            if ( j == nElems )
-                return false;
-            else {
+            if ( value == a[j] ) {
                 for ( int k = j; k < nElems; k++ ) {
                     a[k] = a[k + 1];
                 }
+                nElems--;
+                return true;
             }
         }
+        return false;
     }
 
     public void display() {
         for ( int j = 0; j < nElems; j++ ) {
-            System.out.println( a[j] + " " + "\n" );
+            System.out.print( a[j] + " ");
         }
+        System.out.println();
+    }
+
+    public static void main( String args[] ) {
+        int maxSize = 100;
+        HighArray arr = new HighArray( maxSize );
+
+        arr.insert(77);
+        arr.insert(99);
+        arr.insert(44);
+        arr.insert(55);
+        arr.insert(22);
+        arr.insert(88);
+        arr.insert(11);
+        arr.insert(00);
+        arr.insert(66);
+        arr.insert(33);
+
+        arr.display();
+
+        int searchKey = 35;
+        String findString = ( arr.find( searchKey ) ) ? "Found " + searchKey : "Can't find " + searchKey;
+        System.out.println( findString );
+
+        arr.delete(00);
+        arr.delete(55);
+        arr.delete(99);
+
+        arr.display();
+
     }
 }
