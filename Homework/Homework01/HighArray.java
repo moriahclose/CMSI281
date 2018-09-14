b942087df205793fc86ad6a11d08bb9bc8e2e85c
Moriah Tolliver <mtollive@lion.lmu.edu>
2018-09-14 12:01:15 -0700

    added HighArrayApp.java

--- Homework/Homework01/HighArray.java
+++ Homework/Homework01/HighArray.java
@@ -47,33 +47,4 @@ class HighArray {
         }
         System.out.println();
     }
-
-    public static void main( String args[] ) {
-        int maxSize = 100;
-        HighArray arr = new HighArray( maxSize );
-
-        arr.insert(77);
-        arr.insert(99);
-        arr.insert(44);
-        arr.insert(55);
-        arr.insert(22);
-        arr.insert(88);
-        arr.insert(11);
-        arr.insert(00);
-        arr.insert(66);
-        arr.insert(33);
-
-        arr.display();
-
-        int searchKey = 35;
-        String findString = ( arr.find( searchKey ) ) ? "Found " + searchKey : "Can't find " + searchKey;
-        System.out.println( findString );
-
-        arr.delete(00);
-        arr.delete(55);
-        arr.delete(99);
-
-        arr.display();
-
-    }
 }
