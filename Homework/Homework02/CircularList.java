d52137529b7f8e4d68575b613793ebdc29ddfb7f
Moriah Tolliver <mtollive@lion.lmu.edu>
2018-10-11 17:04:12 -0700

    changed name of circular list

---
+++ Homework/Homework02/CircularList.java
@@ -0,0 +1,11 @@
+/*********************************************************
+Author: Robert Lafore
+Editors: Moriah Tolliver and Tapiwa Tafa
+Purpose: Demonstrates circular linked list
+**********************************************************/
+public class CircularList {
+
+      public CircularList() {
+          super();
+      }
+  }
