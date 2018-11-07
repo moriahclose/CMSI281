a5e41a2c6dac470a81c4ee753cba6825101d1d48
Moriah Tolliver <mtollive@lion.lmu.edu>
2018-11-06 19:07:51 -0800

    initial upload of Simon.java

---
+++ Homework/Homework03/Simon.java
@@ -0,0 +1,49 @@
+/***********************************************************************
+Authors: Moriah Tolliver and Tapiwa Tafa
+Purpose: run game "Simon" with user input 
+*************************************************************************/
+import java.util.*;
+
+class Simon {
+
+    public static void main (String[] args) {
+        Character[] colorInts  = {'R', 'G', 'B', 'Y'};
+
+        String sequence = "";
+        String backspaceString = "";
+
+        System.out.println( "Welcome to Simon!" );
+
+        boolean correctGuess = true;
+
+        Scanner inputScanner = new Scanner(System.in);
+        String inputGuess = "";
+
+        while ( correctGuess ) {
+            int addedCharInt = (int)(Math.floor( Math.random() * 4 ));
+            sequence += colorInts[addedCharInt] + " ";
+
+            System.out.println( "The sequence is: " );
+            for ( char color : sequence.toCharArray() ) {
+                try {
+                    Thread.sleep( 1000 );
+                }
+                catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+                System.out.print( color );
+            }
+            System.out.println("\n Enter the sequence: " );
+            inputGuess = inputScanner.next();
+
+            if ( inputGuess.equals(sequence) ) {
+                correctGuess = true;
+            }
+            else {
+                correctGuess = false;
+                System.out.println( "Oops! The sequence was " + sequence + "but you typed " + inputGuess);
+            }
+
+        }
+    }
+}
